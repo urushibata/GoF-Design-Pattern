6f845cd510a8493dc6207d79c5b48fdc7f78c064
urushibata <urush1bata.lala@gmail.com>
2013-04-11 00:39:15 +0900

    add comment

--- desigpattern/src/command/Main.java
+++ desigpattern/src/command/Main.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package command;
 
@@ -22,65 +22,96 @@ import command.drawer.DrawCommand;
 
 /**
  * @author urushibata
- * 
+ *
  */
 public class Main extends JFrame implements ActionListener,
 		MouseMotionListener, WindowListener {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 6430184986003295769L;
 	// 描画履歴
 	private MacroCommand history = new MacroCommand();
 	// 描画領域
 	private DrawCanvas canvas = new DrawCanvas(400, 400, history);
-	// 消去ボタン
+	// クリアボタン
 	private JButton clearButton = new JButton("clear");
 
 	/**
-	 * @param args
+	 * @param title
+	 *            ウィンドウのタイトル
 	 */
 	public Main(String title) {
+		// スーパクラスのコンストラクタ
 		super(title);
 
-		this.addWindowListener(this);
+		// JFrame→Frame→Windowスーパクラスのメソッド
+		// ウィンドウにリスナー機能を実装する。無くても動く！！
+		// this.addWindowListener(this);
+		// canvasにMouseMotionListenerを持たせる。
 		canvas.addMouseMotionListener(this);
+		// clearButtonにMouseActionListenerを持たせる。
 		clearButton.addActionListener(this);
 
 		// Boxクラスはコンポーネントを作成するクラス
+		// addメソッドでコンポーネント上にコンポーネントを作成する。
 		// X_AXIS コンポーネントを左から右に並べて表示する。
 		Box buttonBox = new Box(BoxLayout.X_AXIS);
 		buttonBox.add(clearButton);
+		System.out.println("ボタンコンポーネント上にクリアボタン作成");
+
 		// Y_AXIS コンポーネントを上から下に並べて表示する。
 		Box mainBox = new Box(BoxLayout.Y_AXIS);
 		mainBox.add(buttonBox);
 		mainBox.add(canvas);
-		getContentPane().add(mainBox);
+		System.out.println("メインコンポーネント上にボタンコンポーネントとキャンバス作成");
 
+		// getContentPaneでルートコンポーネントを作成する。
+		getContentPane().add(mainBox);
+		System.out.println("ルートコンポーネント上にメインコンポーネントを作成");
+
+		// Windowクラスのメソッド(JFrameを継承しているため使用可能)
+		// 各コンポーネントよりウィンドウのサイズを自動調整する。これがないと画面は最少サイズになる。
 		pack();
+		// ウィンドウ表示
 		setVisible(true);
+		System.out.println("ウィンドウ表示");
 	}
 
 	/*
 	 * (非 Javadoc)
-	 * 
+	 *
 	 * @see
 	 * java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 * addActionListenerされたコンポーネントを監視しイベントが発生したら処理をする。
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		// アクションイベント発生元がクリアボタンの場合
 		if (e.getSource() == clearButton) {
+			// MacroCommandに溜まったイベント履歴を全てクリアする。
 			history.clear();
+			// 再描画
+			// DrawCanvas→Canvas→Componentスーパクラスのメソッド
 			canvas.repaint();
-			System.out.println("Clear Event");
+			System.out.println("Clear Event execute success");
 		}
 	}
 
+	/*
+	 * (非 Javadoc)
+	 *
+	 * @see
+	 * java.awt.event.ActionListener#mouseDragged(java.awt.event.ActionEvent)
+	 * addMouseMotionListenerされたコンポーネントを監視しドラッグイベントが発生したら処理をする。
+	 */
 	@Override
 	public void mouseDragged(MouseEvent e) {
+		// DrawCommandインスタンスを作成しhistory(MacroCommand)に格納する。
 		Command cmd = new DrawCommand(this.canvas, e.getPoint());
 		history.append(cmd);
+		// コマンドを実行
 		cmd.execute();
 	}
 
@@ -117,6 +148,11 @@ public class Main extends JFrame implements ActionListener,
 	public void windowDeactivated(WindowEvent e) {
 	}
 
+	/*
+	 * (非 Javadoc)
+	 *
+	 * メインクラス
+	 */
 	public static void main(String[] args) {
 		System.out.println("command pattern Strat");
 		new Main("Command Pattern Sample");
--- desigpattern/src/command/cmmand/MacroCommand.java
+++ desigpattern/src/command/cmmand/MacroCommand.java
@@ -1,20 +1,21 @@
 /**
- * 
+ *
  */
 package command.cmmand;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Iterator;
 
 /**
  * @author urushibata
- * 
+ *
  */
 public class MacroCommand implements Command {
 
 	/*
 	 * (非 Javadoc)
-	 * 
+	 *
 	 * @see command.cmmand.Command#execute()
 	 */
 	// 命令の集合(スタック:LIFO)
@@ -24,29 +25,40 @@ public class MacroCommand implements Command {
 	@Override
 	public void execute() {
 		Iterator<Command> it = commands.iterator();
-		while(it.hasNext()){
-			((Command)it.next()).execute();
+		while (it.hasNext()) {
+			((Command) it.next()).execute();
 		}
 	}
-	
-	//追加
-	public void append(Command cmd){
-		if(cmd != this){
+
+	/**
+	 * @param cmd DrawComandインスタンスがCommand型で渡される。
+	 * コマンドを履歴に追加する。
+	 */
+	public void append(Command cmd) {
+		// コマンドが自分自身以外の場合
+		if (cmd != this) {
 			commands.push(cmd);
+			System.out.println("MacroCommandにコマンドを追加");
+			System.out.println("MacroCommandの中身：" + Arrays.deepToString(commands.toArray()));
 		}
 	}
 
-	//最後の命令を削除
-	public void undo(){
-		if(!commands.isEmpty()){
+	// 最後の命令を削除
+	public void undo() {
+		System.out.println("Undo前MacroCommandの中身：" + commands);
+		// コマンドが自分自身以外の場合
+		if (!commands.isEmpty()) {
 			commands.pop();
+			System.out.println("Undo後MacroCommandの中身：" + commands);
 		}
 	}
 
-	//全部削除
-	public void clear(){
-		if(!commands.isEmpty()){
+	// 全部削除
+	public void clear() {
+		System.out.println("Clear前MacroCommandの中身：" + commands);
+		if (!commands.isEmpty()) {
 			commands.clear();
+			System.out.println("Clear前MacroCommandの中身：" + commands);
 		}
 	}
 }
